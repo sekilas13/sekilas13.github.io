ae57d6141bb3d335fc3c2cedf0208c52e3e0a01c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-09-18 19:09:07 +0700

    Prototipe KataOrang.js

--- src/App/Components/KataOrang.js
+++ src/App/Components/KataOrang.js
@@ -2,10 +2,33 @@ import React, { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 
 function KataOrang() {
+  const [index, setIndex] = useState(4);
+
   const [ukuran, setUkuran] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  const item = [
+    {
+      alt: "Gambar 1",
+      capt: [
+        "Edwin",
+        "Bagus, karna mereka slalu membuat penemuan yang mengejutkan",
+      ],
+    },
+    {
+      alt: "Gambar 2",
+      capt: [
+        "Luthfi Jatmiko",
+        "Eskulnya santai sih, bercanda juga iya. Bukanya ga serius tapi biar nyaman aja",
+      ],
+    },
+  ];
 
   useEffect(() => {
     const curr = document.querySelector("#KataOrang");
@@ -20,47 +43,20 @@ function KataOrang() {
 
   return (
     <section id="KataOrang">
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            // src="holder.js/800x400?text=First slide&bg=373940"
-            src={`https://dummyimage.com/${ukuran.width}x${ukuran.height}/373940/fff&text=+`}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            // src="holder.js/800x400?text=Second slide&bg=282c34"
-            src={`https://dummyimage.com/${ukuran.width}x${ukuran.height}/282c34/fff&text=+`}
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            // src="holder.js/800x400?text=Third slide&bg=20232a"
-            src={`https://dummyimage.com/${ukuran.width}x${ukuran.height}/20232a/fff&text=+`}
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {item.map((key, i) => (
+          <Carousel.Item key={i}>
+            <img
+              className="d-block w-100"
+              src={`https://dummyimage.com/${ukuran.width}x${ukuran.height}/373940/fff&text=+`}
+              alt={key.alt}
+            />
+            <Carousel.Caption>
+              <h3>{key.capt[0]}</h3>
+              <p>{key.capt[1]}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </section>
   );
