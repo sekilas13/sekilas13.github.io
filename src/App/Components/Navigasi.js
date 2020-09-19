53d8ff157df89feeaeb089c8a6e6740cef8d32f7
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-09-19 12:20:27 +0700

    Penyempurnaan navigasi

--- src/App/Components/Navigasi.js
+++ src/App/Components/Navigasi.js
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Navigasi() {
   const ref = useRef();
+  const [state, setState] = useState({ expanded: false });
 
   const handleBrand = (e) => {
     e.preventDefault();
@@ -10,37 +11,66 @@ function Navigasi() {
     Links.forEach((el) => {
       el.classList.remove("active");
     });
-    window.scrollTo(0, 0);
+    if (state.expanded) {
+      setState({ expanded: false });
+      setTimeout(() => window.scrollTo(0, 0), 150);
+    } else {
+      window.scrollTo(0, 0);
+    }
   };
 
   const handleLink = (e) => {
     e.preventDefault();
 
-    const el = document.querySelector(e.target.id.split("-link")[0]);
-    const tujuan = el.offsetTop - ref.current.getBoundingClientRect().height;
-    window.scrollTo(0, tujuan);
+    setState({ expanded: false });
+    const el = document.querySelector(e.target.id);
+    if (state.expanded) {
+      setTimeout(() => {
+        const tujuan =
+          el.offsetTop - ref.current.getBoundingClientRect().height;
+        window.scrollTo(0, tujuan);
+      }, 150);
+    } else {
+      const tujuan = el.offsetTop - ref.current.getBoundingClientRect().height;
+      window.scrollTo(0, tujuan);
+    }
   };
 
-  const Link = ["Deskripsi", "Gambar"];
+  const Link = [
+    { nama: "Deskripsi", to: "#deskripsi" },
+    { nama: "Pandangan Orang", to: "#KataOrang" },
+    { nama: "Gambar", to: "#gambar" },
+  ];
 
   return (
-    <Navbar bg="light" variant="light" sticky="top" ref={ref}>
+    <Navbar
+      bg="light"
+      variant="light"
+      sticky="top"
+      expand="lg"
+      expanded={state.expanded}
+      onToggle={() => setState({ expanded: state.expanded ? false : true })}
+      ref={ref}
+    >
       <Container>
         <Navbar.Brand href="#" onClick={handleBrand}>
           KIR
         </Navbar.Brand>
-        <Nav className="my-auto">
-          {Link.map((nama, i) => (
-            <Nav.Link
-              key={i}
-              id={`#${nama.toLowerCase()}-link`}
-              href={`#${nama.toLowerCase()}`}
-              onClick={handleLink}
-            >
-              {nama}
-            </Nav.Link>
-          ))}
-        </Nav>
+        <Navbar.Toggle aria-controls="navigasi-nav" />
+        <Navbar.Collapse id="navigasi-nav">
+          <Nav className="ml-auto text-center">
+            {Link.map((link, i) => (
+              <Nav.Link
+                key={i}
+                href={link.to}
+                id={link.to}
+                onClick={handleLink}
+              >
+                {link.nama}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
