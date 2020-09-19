6611fd0caa595936783b64a3591d9e0fdfd8f230
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-09-19 14:24:16 +0700

    Updates

---
+++ precache-manifest.1066f9941223a2aa819d5075e3be8412.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b93c7c960b1ea3c56ec6b8967d41658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad2084dc20c34983d35",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a3a9e7a5b353c216cdcf",
+    "url": "/static/css/main.ac48e20e.chunk.css"
+  },
+  {
+    "revision": "aad2084dc20c34983d35",
+    "url": "/static/js/2.ba4b5276.chunk.js"
+  },
+  {
+    "revision": "00bd0a57b35bcb95c2bf686d809b5abd",
+    "url": "/static/js/2.ba4b5276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a9e7a5b353c216cdcf",
+    "url": "/static/js/main.4dd4d606.chunk.js"
+  },
+  {
+    "revision": "7d40de8657fe7bf1a95e",
+    "url": "/static/js/runtime-main.38af76df.js"
+  },
+  {
+    "revision": "ebd70c1d7030b3238a86ec562cd044f4",
+    "url": "/static/media/1.ebd70c1d.jpg"
+  },
+  {
+    "revision": "ba87505d27c0a0faa3af3dca0747c874",
+    "url": "/static/media/2.ba87505d.jpg"
+  },
+  {
+    "revision": "7c62637281a62624a55225d240a16fdf",
+    "url": "/static/media/3.7c626372.jpg"
+  },
+  {
+    "revision": "eed83b2ba954691440329435723bd544",
+    "url": "/static/media/4.eed83b2b.jpg"
+  },
+  {
+    "revision": "dfed00f9dbfcf1160e6eb92c35795fc2",
+    "url": "/static/media/5.dfed00f9.jpg"
+  },
+  {
+    "revision": "449e5b0b7642f8e6fd776b10dc6da031",
+    "url": "/static/media/6.449e5b0b.jpg"
+  },
+  {
+    "revision": "b32203b6bdb464d308ba00c8b3c16774",
+    "url": "/static/media/KIR.b32203b6.png"
+  }
+]);
