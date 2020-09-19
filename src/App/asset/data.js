1de63444805b9484a0bb189e4e290d7f42ba3394
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-09-19 14:21:12 +0700

    Mengubah data.js

--- src/App/asset/data.js
+++ src/App/asset/data.js
@@ -1,16 +1,16 @@
 export default [
   {
-    alt: "Gambar 1",
+    alt: "Gambar background Miko 1",
     capt: [
       "Edwin",
-      "Bagus, karna mereka slalu membuat penemuan yang mengejutkan",
+      '"Bagus, karna mereka slalu membuat penemuan yang mengejutkan"',
     ],
   },
   {
-    alt: "Gambar 2",
+    alt: "Gambar background Miko 2",
     capt: [
       "Luthfi Jatmiko",
-      "Eskulnya santai sih, bercanda juga iya. Bukanya ga serius tapi biar nyaman aja",
+      '"Eskulnya santai sih, bercanda juga iya. Bukannya ga serius tapi biar nyaman aja"',
     ],
   },
 ];
