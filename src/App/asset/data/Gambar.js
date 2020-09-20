f6c603052b34b173b8b54c16f9e64446638546fb
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-09-21 06:51:26 +0700

    Memibuat file baru untuk memisahkan data

---
+++ src/App/asset/data/Gambar.js
@@ -0,0 +1,35 @@
+import img1 from "../Img/1.jpg";
+import img2 from "../Img/2.jpg";
+import img3 from "../Img/3.jpg";
+import img4 from "../Img/4.jpg";
+import img5 from "../Img/5.jpg";
+import img6 from "../Img/6.jpg";
+
+export default [
+  {
+    src: img1,
+    alt: "Demonstrasi roket air pada saat pengenalan eskul",
+  },
+  {
+    src: img2,
+    alt:
+      "Eksperimen odol gajah yang reaksinya mengeluarkan odol yang ada di botol",
+  },
+  {
+    src: img3,
+    alt: "Anak kelas 7 ikut serta mencoba eksperimen odol gajah",
+  },
+  {
+    src: img4,
+    alt: "Anggota KIR yang melakukan eksperimen 'Nafas api'",
+  },
+  {
+    src: img5,
+    alt:
+      "Anggota eksperimen meniup selang yg berisi tepung maizena untuk menghasilkan nafas api",
+  },
+  {
+    src: img6,
+    alt: "Anggota eksperimen sedang menyiapkan bahan dan alat",
+  },
+];
