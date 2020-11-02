25a3927a4443a72f85c843ca254624b416f691b3
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-02 07:30:59 +0700

    Memperbarui versi react

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./main.css";
 import "./responsive.css";
 
+import reportWebVitals from "./reportWebVitals";
+
 ReactDOM.render(<App />, document.querySelector("div#content"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
