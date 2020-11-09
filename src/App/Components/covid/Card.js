5cc8cf8ccd655ba1865631eb88c71a022ed2de45
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-09 11:55:33 +0700

    Prototype tata letak card

--- src/App/Components/covid/Card.js
+++ src/App/Components/covid/Card.js
@@ -1,5 +1,32 @@
+import { Card as CardBootstrap, Row, Container, Col } from "react-bootstrap";
+
 function Card() {
-  return <></>;
+  return (
+    <Container fluid>
+      <Row className="mt-4 justify-content-center">
+        <Col md={3} sm={5}>
+          <CardBootstrap>
+            <CardBootstrap.Body></CardBootstrap.Body>
+          </CardBootstrap>
+        </Col>
+        <Col md={3} sm={5}>
+          <CardBootstrap>
+            <CardBootstrap.Body></CardBootstrap.Body>
+          </CardBootstrap>
+        </Col>
+        <Col md={3} sm={5}>
+          <CardBootstrap>
+            <CardBootstrap.Body></CardBootstrap.Body>
+          </CardBootstrap>
+        </Col>
+        <Col md={3} sm={5}>
+          <CardBootstrap>
+            <CardBootstrap.Body></CardBootstrap.Body>
+          </CardBootstrap>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Card;
