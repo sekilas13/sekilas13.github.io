40e49ecce5c28c2db257a03f98644234c1d593f5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-09 11:18:29 +0700

    Menambahkan axios untuk menambahkan data

--- src/App/Components/covid/index.js
+++ src/App/Components/covid/index.js
@@ -1,7 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useContext, useCallback } from "react";
+import { Context } from "../../utils/stateProvider";
+import { observer } from "mobx-react";
 import Card from "./Card";
+import axios from "axios";
 
 function Covid() {
+  const store = useContext(Context);
+
+  const set = useCallback((data, index) => store.setDataCovid(data, index), [
+    store,
+  ]);
+
+  useEffect(() => {
+    axios
+      .get("https://indonesia-covid-19.mathdro.id/api/")
+      .then((data) => data.data)
+      .then((r) => set(r, "main"));
+  }, [set]);
+
   return (
     <Fragment>
       <Card />
@@ -9,4 +25,4 @@ function Covid() {
   );
 }
 
-export default Covid;
+export default observer(Covid);
