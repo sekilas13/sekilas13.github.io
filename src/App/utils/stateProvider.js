557f07cacfbe299b85af2971135d013fd61da6fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-09 08:20:23 +0700

    Menambah state untuk data covid

--- src/App/utils/stateProvider.js
+++ src/App/utils/stateProvider.js
@@ -7,6 +7,8 @@ const Provider = ({ children }) => {
   const store = useLocalObservable(() => ({
     theme: "light",
     setTheme: (theme) => (store.theme = theme),
+    dataCovid: null,
+    setDataCovid: (arr) => (store.dataCovid = arr),
   }));
 
   return <Context.Provider value={store}>{children}</Context.Provider>;
