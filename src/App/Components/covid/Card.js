2c1097881e3e3564e560d48cbb0f05f66f52b828
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-10 17:03:49 +0700

    Prototipe awal untuk tampilan data

--- src/App/Components/covid/Card.js
+++ src/App/Components/covid/Card.js
@@ -1,32 +1,75 @@
+import { useContext } from "react";
+import { Context } from "../../utils/stateProvider";
+import { observer } from "mobx-react";
 import { Card as CardBootstrap, Row, Container, Col } from "react-bootstrap";
+// import {} from "@fortawesome/free-solid-svg-icons";
 
 function Card() {
+  const store = useContext(Context);
+  const main = store.dataMain;
+
   return (
-    <Container fluid>
-      <Row className="mt-4 justify-content-center">
-        <Col md={3} sm={5}>
-          <CardBootstrap>
-            <CardBootstrap.Body></CardBootstrap.Body>
-          </CardBootstrap>
-        </Col>
-        <Col md={3} sm={5}>
-          <CardBootstrap>
-            <CardBootstrap.Body></CardBootstrap.Body>
-          </CardBootstrap>
-        </Col>
-        <Col md={3} sm={5}>
-          <CardBootstrap>
-            <CardBootstrap.Body></CardBootstrap.Body>
-          </CardBootstrap>
-        </Col>
-        <Col md={3} sm={5}>
-          <CardBootstrap>
-            <CardBootstrap.Body></CardBootstrap.Body>
-          </CardBootstrap>
-        </Col>
-      </Row>
-    </Container>
+    <section className="covid">
+      <Container fluid>
+        {main && (
+          <Row className="mt-4 justify-content-center">
+            <Col md={3} sm={5}>
+              <CardBootstrap>
+                <CardBootstrap.Body>
+                  <Row className="justify-content-center h-100 d-flex">
+                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={8} className="text-center align-self-center">
+                      <h4>Positif</h4>
+                      <h3>{main.jumlahKasus.toLocaleString()} Orang</h3>
+                    </Col>
+                  </Row>
+                </CardBootstrap.Body>
+              </CardBootstrap>
+            </Col>
+            <Col md={3} sm={5}>
+              <CardBootstrap>
+                <CardBootstrap.Body>
+                  <Row className="justify-content-center h-100 d-flex">
+                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={8} className="text-center align-self-center">
+                      <h4>Dirawat</h4>
+                      <h3>{main.perawatan.toLocaleString()} Orang</h3>
+                    </Col>
+                  </Row>
+                </CardBootstrap.Body>
+              </CardBootstrap>
+            </Col>
+            <Col md={3} sm={5}>
+              <CardBootstrap>
+                <CardBootstrap.Body>
+                  <Row className="justify-content-center h-100 d-flex">
+                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={8} className="text-center align-self-center">
+                      <h4>Sembuh</h4>
+                      <h3>{main.sembuh.toLocaleString()} Orang</h3>
+                    </Col>
+                  </Row>
+                </CardBootstrap.Body>
+              </CardBootstrap>
+            </Col>
+            <Col md={3} sm={5}>
+              <CardBootstrap>
+                <CardBootstrap.Body>
+                  <Row className="justify-content-center h-100 d-flex">
+                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={8} className="text-center align-self-center">
+                      <h4>Meninggal</h4>
+                      <h3>{main.meninggal.toLocaleString()} Orang</h3>
+                    </Col>
+                  </Row>
+                </CardBootstrap.Body>
+              </CardBootstrap>
+            </Col>
+          </Row>
+        )}
+      </Container>
+    </section>
   );
 }
 
-export default Card;
+export default observer(Card);
