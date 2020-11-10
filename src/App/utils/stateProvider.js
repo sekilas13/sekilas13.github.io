111138f2cc58e95d1d32b5d5c0f206aa363e0c6e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-10 17:02:09 +0700

    Mengubah struktur data dan setter

--- src/App/utils/stateProvider.js
+++ src/App/utils/stateProvider.js
@@ -7,8 +7,9 @@ const Provider = ({ children }) => {
   const store = useLocalObservable(() => ({
     theme: "light",
     setTheme: (theme) => (store.theme = theme),
-    dataCovid: { main: undefined, provinsi: undefined },
-    setDataCovid: (arr, index) => (store.dataCovid[index] = arr),
+    dataMain: null,
+    dataProvinsi: [],
+    setData: (arr, index) => (store[`data${index}`] = arr),
   }));
 
   return <Context.Provider value={store}>{children}</Context.Provider>;
