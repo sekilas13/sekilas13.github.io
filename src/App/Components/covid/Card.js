a1f6538f4644e114f615358e66701c8366b78d60
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-10 23:26:36 +0700

    Menambahkan font awesome

--- src/App/Components/covid/Card.js
+++ src/App/Components/covid/Card.js
@@ -2,7 +2,13 @@ import { useContext } from "react";
 import { Context } from "../../utils/stateProvider";
 import { observer } from "mobx-react";
 import { Card as CardBootstrap, Row, Container, Col } from "react-bootstrap";
-// import {} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faVirus,
+  faHospital,
+  faHandHoldingMedical,
+  faSkullCrossbones,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Card() {
   const store = useContext(Context);
@@ -17,7 +23,9 @@ function Card() {
               <CardBootstrap>
                 <CardBootstrap.Body>
                   <Row className="justify-content-center h-100 d-flex">
-                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={4} className="text-center align-self-center">
+                      <FontAwesomeIcon icon={faVirus} size="6x" />
+                    </Col>
                     <Col md={8} className="text-center align-self-center">
                       <h4>Positif</h4>
                       <h3>{main.jumlahKasus.toLocaleString()} Orang</h3>
@@ -30,7 +38,9 @@ function Card() {
               <CardBootstrap>
                 <CardBootstrap.Body>
                   <Row className="justify-content-center h-100 d-flex">
-                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={4} className="text-center align-self-center">
+                      <FontAwesomeIcon icon={faHospital} size="6x" />
+                    </Col>
                     <Col md={8} className="text-center align-self-center">
                       <h4>Dirawat</h4>
                       <h3>{main.perawatan.toLocaleString()} Orang</h3>
@@ -43,7 +53,9 @@ function Card() {
               <CardBootstrap>
                 <CardBootstrap.Body>
                   <Row className="justify-content-center h-100 d-flex">
-                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={4} className="text-center align-self-center">
+                      <FontAwesomeIcon icon={faHandHoldingMedical} size="6x" />
+                    </Col>
                     <Col md={8} className="text-center align-self-center">
                       <h4>Sembuh</h4>
                       <h3>{main.sembuh.toLocaleString()} Orang</h3>
@@ -56,7 +68,9 @@ function Card() {
               <CardBootstrap>
                 <CardBootstrap.Body>
                   <Row className="justify-content-center h-100 d-flex">
-                    <Col md={4} className="text-center align-self-center"></Col>
+                    <Col md={4} className="text-center align-self-center">
+                      <FontAwesomeIcon icon={faSkullCrossbones} size="6x" />
+                    </Col>
                     <Col md={8} className="text-center align-self-center">
                       <h4>Meninggal</h4>
                       <h3>{main.meninggal.toLocaleString()} Orang</h3>
