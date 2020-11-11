d0308a3af620c49aa19d1b332c3608307dfd8572
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-11 19:16:18 +0700

    Menambahkan table

--- src/App/Components/covid/Bagan.js
+++ src/App/Components/covid/Bagan.js
@@ -1,12 +1,28 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { Context } from "../../utils/stateProvider";
+import { Table } from "react-bootstrap";
 import { observer } from "mobx-react";
 
 function Bagan() {
   const store = useContext(Context);
   const prov = store.dataProvinsi;
 
-  return <></>;
+  return (
+    <Fragment>
+      {prov && (
+        <Table striped bordered variant={store.theme}>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+        </Table>
+      )}
+    </Fragment>
+  );
 }
 
 export default observer(Bagan);
