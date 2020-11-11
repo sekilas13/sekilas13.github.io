7c3da77cb185a1a972ad6b75b1c8d77a8c5ce32b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-11 15:17:28 +0700

    Memanggil element meta untuk warna supaya bisa diubah

--- src/App/hooks/useDarkMode.js
+++ src/App/hooks/useDarkMode.js
@@ -3,18 +3,40 @@ import { Context } from "../utils/stateProvider";
 
 export const useDarkMode = () => {
   const store = useContext(Context);
+
+  const themeColor = document.querySelector('meta[name="theme-color"]');
+  const msApp = document.querySelector('meta[name="msapplication-TileColor"]');
+
+  const light = "#f0efeb";
+  const dark = "#323234";
+
   const setMode = (mode) => {
     window.localStorage.setItem("theme", mode);
     store.setTheme(mode);
   };
 
   const themeToggler = () => {
-    store.theme === "light" ? setMode("dark") : setMode("light");
+    if (store.theme === "light") {
+      setMode("dark");
+      themeColor.content = dark;
+      msApp.content = dark;
+    } else {
+      setMode("light");
+      themeColor.content = light;
+      msApp.content = light;
+    }
   };
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme");
     localTheme ? store.setTheme(localTheme) : setMode("light");
+    if (localTheme && localTheme === "light") {
+      themeColor.content = light;
+      msApp.content = light;
+    } else {
+      themeColor.content = dark;
+      msApp.content = dark;
+    }
     // eslint-disable-next-line
   }, []);
 
