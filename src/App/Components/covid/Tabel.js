8e5dfe065d9509946575656a03ec202823b32edf
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-11 20:41:22 +0700

    Memindahkan Row ke dalam conditional render

--- src/App/Components/covid/Tabel.js
+++ src/App/Components/covid/Tabel.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { Context } from "../../utils/stateProvider";
 import { Table, Row, Col } from "react-bootstrap";
 import { observer } from "mobx-react";
@@ -9,36 +9,38 @@ function Tabel() {
 
   return (
     <section id="#provinsi">
-      <Row className="justify-content-center mt-4 mb-2">
-        <Col md={6}>
-          <h1 className="text-center">Data Provinsi</h1>
-        </Col>
-      </Row>
       {store.dataMain && prov && (
-        <Table striped bordered hover responsive variant={store.theme}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Provinsi</th>
-              <th>Positif</th>
-              <th>Dirawat</th>
-              <th>Sembuh</th>
-            </tr>
-          </thead>
-          <tbody>
-            {prov
-              .filter((d) => d.provinsi !== "Indonesia")
-              .map((d, i) => (
-                <tr key={d.kodeProvi}>
-                  <td>{i + 1}</td>
-                  <td>{d.provinsi}</td>
-                  <td>{d.kasusPosi.toLocaleString()}</td>
-                  <td>{d.kasusSemb.toLocaleString()}</td>
-                  <td>{d.kasusMeni.toLocaleString()}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
+        <Fragment>
+          <Row className="justify-content-center mt-4 mb-2">
+            <Col md={6}>
+              <h1 className="text-center">Data Provinsi</h1>
+            </Col>
+          </Row>
+          <Table striped bordered hover responsive variant={store.theme}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Provinsi</th>
+                <th>Positif</th>
+                <th>Dirawat</th>
+                <th>Sembuh</th>
+              </tr>
+            </thead>
+            <tbody>
+              {prov
+                .filter((d) => d.provinsi !== "Indonesia")
+                .map((d, i) => (
+                  <tr key={d.kodeProvi}>
+                    <td>{i + 1}</td>
+                    <td>{d.provinsi}</td>
+                    <td>{d.kasusPosi.toLocaleString()}</td>
+                    <td>{d.kasusSemb.toLocaleString()}</td>
+                    <td>{d.kasusMeni.toLocaleString()}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </Fragment>
       )}
     </section>
   );
