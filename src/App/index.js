7f7a301631e265aae4d1237602bb877acc9324a5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-11 23:38:35 +0700

    Menambahkan container fluid agar tidak bisa scroll kanan kiri

--- src/App/index.js
+++ src/App/index.js
@@ -23,17 +23,19 @@ function App() {
 
   if (!mountedComponent) {
     return (
-      <div className="row justify-content-center mt-5">
-        <div
-          className="spinner-border text-primary mt-5"
-          style={{
-            fontSize: "2rem",
-            width: "5rem",
-            height: "5rem",
-          }}
-          role="status"
-        >
-          <span className="sr-only">Loading...</span>
+      <div className="container-fluid">
+        <div className="row justify-content-center mt-5">
+          <div
+            className="spinner-border text-primary mt-5"
+            style={{
+              fontSize: "2rem",
+              width: "5rem",
+              height: "5rem",
+            }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
       </div>
     );
