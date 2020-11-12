8c6c5f98474944abd5adffc73ff4424105880507
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-12 09:18:16 +0700

    Menambahkan spinner dan keterangannya

--- src/App/Components/covid/custom/Loading.js
+++ src/App/Components/covid/custom/Loading.js
@@ -1,5 +1,33 @@
+import { Card, Row, Col, Spinner } from "react-bootstrap";
+
 function Loading() {
-  return <></>;
+  return (
+    <Row className="justify-content-center mt-3">
+      <Col lg={4} sm={5} md={4}>
+        <Card id="loading">
+          <Card.Body>
+            <Row className="justify-content-center">
+              <Spinner
+                animation="border"
+                role="status"
+                variant="primary"
+                style={{
+                  fontSize: "2rem",
+                  width: "5rem",
+                  height: "5rem",
+                }}
+              />
+            </Row>
+            <Row className="justify-content-center mt-3">
+              <p className="text-center">
+                Sedang mengambil data, mohon tunggu sebentar
+              </p>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 }
 
 export default Loading;
