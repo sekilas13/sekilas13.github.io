f84efe067186d33f57e906222afa4477a96f51c7
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-15 00:31:40 +0700

    Memanggil GambarMap untuk lazy loading

--- src/App/Components/main/Gambar.js
+++ src/App/Components/main/Gambar.js
@@ -1,21 +1,20 @@
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Suspense, lazy } from "react";
+import { Container } from "react-bootstrap";
 import { SRLWrapper } from "simple-react-lightbox";
-import gambar from "../../assets/data/Gambar";
+import Loading from "../../Custom/Loading";
+
+const GambarMap = lazy(() => import("./lazy/GambarMap"));
 
 function Gambar() {
   return (
     <section id="gambar">
-      <SRLWrapper>
-        <Container className="pt-4">
-          <Row>
-            {gambar.map((g, i) => (
-              <Col md={4} key={i}>
-                <Image src={g.src} alt={g.alt} className="img-thumbnail mt-3" />
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </SRLWrapper>
+      <Suspense fallback={<Loading />}>
+        <SRLWrapper>
+          <Container className="pt-4">
+            <GambarMap />
+          </Container>
+        </SRLWrapper>
+      </Suspense>
     </section>
   );
 }
