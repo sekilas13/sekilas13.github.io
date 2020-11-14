dc377d08d06487adcdd624828884d0bd6f872820
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-14 23:50:36 +0700

    Mengganti mount component menjadi React.lazy

--- src/App/index.js
+++ src/App/index.js
@@ -1,47 +1,24 @@
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useContext, Suspense, lazy } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./assets/Theme";
 import { GlobalStyles } from "./assets/GlobalStyles";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Components/main";
-import Covid from "./Components/covid";
+import Loading from "./Custom/Loading";
 import Navigasi from "./Components/Navigasi";
 import NotFound from "./Components/NotFound";
 import SimpleReactLightbox from "simple-react-lightbox";
 import { Context } from "./utils/stateProvider";
 import { observer } from "mobx-react";
+const Main = lazy(() => import("./Components/main"));
+const Covid = lazy(() => import("./Components/covid"));
 
 function App() {
   const store = useContext(Context);
-  const [mountedComponent, SET_mountedComponent] = useState(false);
-
-  useEffect(() => {
-    SET_mountedComponent(true);
-    // eslint-disable-next-line
-  }, []);
 
   const themeMode = store.theme === "light" ? lightTheme : darkTheme;
 
-  if (!mountedComponent) {
-    return (
-      <div className="container-fluid">
-        <div className="row justify-content-center mt-5">
-          <div
-            className="spinner-border text-primary mt-5"
-            style={{
-              fontSize: "2rem",
-              width: "5rem",
-              height: "5rem",
-            }}
-            role="status"
-          >
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
+  return (
+    <Suspense fallback={<Loading />}>
       <ThemeProvider theme={themeMode}>
         <Fragment>
           <GlobalStyles />
@@ -57,8 +34,8 @@ function App() {
           </SimpleReactLightbox>
         </Fragment>
       </ThemeProvider>
-    );
-  }
+    </Suspense>
+  );
 }
 
 export default observer(App);
