21e0aea87a2d496fd70aac31d4f994e14546e29e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-16 22:17:12 +0700

    Menambahkan logika worker.js

---
+++ public/worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "verr-1";
+const urlToCache = ["index.html", "offline.html"];
+
+self.addEventListener("install", (event) => {
+  evemt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Open cache");
+
+      return cache.addAll(urlToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cache.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const whiteList = [];
+  whiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!whiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
