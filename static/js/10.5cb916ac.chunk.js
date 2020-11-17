0fbfec1f599295dd3f0441b6c3b57a43ee2363fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-17 22:34:51 +0700

    Updates

---
+++ static/js/10.5cb916ac.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkir-13"]=this["webpackJsonpkir-13"]||[]).push([[10],{248:function(e,c,t){"use strict";t.r(c);var n=t(4),s=t(0),i=t(203),a=t(75),r=t(69),j=Object(s.lazy)((function(){return t.e(11).then(t.bind(null,242))}));c.default=function(){return Object(n.jsx)("section",{id:"gambar",children:Object(n.jsx)(s.Suspense,{fallback:Object(n.jsx)(r.a,{}),children:Object(n.jsx)(a.a,{children:Object(n.jsx)(i.a,{className:"pt-4",children:Object(n.jsx)(j,{})})})})})}}}]);
+//# sourceMappingURL=10.5cb916ac.chunk.js.map
