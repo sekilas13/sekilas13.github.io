6650282b4b0d34e770e3ed9c9797a3969e3a71fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-11-29 00:31:29 +0700

    Memperbarui penggunaan sitemap

--- src/sitemap-gen.js
+++ src/sitemap-gen.js
@@ -1,11 +1,3 @@
-const es2015 = require("babel-preset-es2015");
-const presetReact = require("babel-preset-react");
-const dynamicImport = require("plugin-proposal-dynamic-import");
-
-require("babel-register")({
-  presets: [es2015, presetReact, dynamicImport],
-});
-
 const router = require("./App/Routing").default;
 const Sitemap = require("react-router-sitemap").default;
 
