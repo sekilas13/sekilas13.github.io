89cedfd7878c12b0e7d7109c8e737c0dfa967f0d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-02 14:54:57 +0700

    Menambahkan animasi blur up kir

--- src/App/Components/main/lazy/JumbonImg.js
+++ src/App/Components/main/lazy/JumbonImg.js
@@ -1,17 +1,41 @@
-import { lazy } from "react";
+import { lazy, useEffect, useState } from "react";
 const Animated = lazy(() =>
   import("react-spring").then((mod) => ({ default: mod.animated.img }))
 );
-// process.env.PUBLIC_URL + "/assets/real/KIR.placeholder.png"
+
+const loadRealImage = (src) =>
+  new Promise((resolve) => {
+    const image = new Image();
+    image.onload = resolve;
+    image.src = src;
+  });
+
+const src = {
+  real: process.env.PUBLIC_URL + "/assets/real/KIR.png",
+  placeholder: process.env.PUBLIC_URL + "/assets/real/KIR.placeholder.png",
+};
 
 function JumboImg({ offset, calc }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(
+    () =>
+      setTimeout(
+        loadRealImage(src.real).then(() => setLoading(false)),
+        500
+      ),
+    []
+  );
+
   return (
     <Animated
-      src={process.env.PUBLIC_URL + "/assets/real/KIR.png"}
+      src={loading ? src.placeholder : src.real}
       className="img-fluid text-center rounded"
       alt="Logo KIR"
       style={{
+        filter: loading ? "blur(5px)" : "none",
         transform: offset.interpolate(calc),
+        transition: "all 0.40s linear",
         overflow: "hidden",
       }}
     />
