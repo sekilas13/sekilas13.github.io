2f826141b1d19a75e3470e5ea5d0f164185b58e9
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-03 15:58:42 +0700

    Mengomentari reporting web vitals karena tidak digunakan

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "./App/utils/stateProvider";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider>
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
