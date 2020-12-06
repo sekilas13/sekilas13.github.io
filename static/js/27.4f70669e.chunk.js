ad14a192f6b05adde77342e4c6790ebaba4408f3
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-06 09:08:45 +0700

    Updates

---
+++ static/js/27.4f70669e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkir-13"]=this["webpackJsonpkir-13"]||[]).push([[27],{283:function(e,t,n){"use strict";n.r(t);var r=n(3),i=n(0),s=Object(i.lazy)((function(){return Promise.resolve().then(n.bind(null,70)).then((function(e){return{default:e.animated.h1}}))}));t.default=function(e){var t=e.offset,n=e.calc2;return Object(r.jsx)(s,{style:{transform:t.interpolate(n),overflow:"hidden"},id:"tsukilas",children:"Sekilas !"})}}}]);
+//# sourceMappingURL=27.4f70669e.chunk.js.map
