c0fa7d165ce3f01ce399b40c2f30bed446788ce5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-22 11:51:37 +0700

    Memindahkan suspense ke fallback loadable component

--- src/App/Components/main/Deskripsi.js
+++ src/App/Components/main/Deskripsi.js
@@ -1,42 +1,40 @@
-import { Suspense } from "react";
 import LazyLoad from "react-lazyload";
 import loadable from "@loadable/component";
-const Content = loadable(() => import("./lazy/DeskripsiContent"));
 
-const Fallback = () => (
-  <div className="container">
-    <div className="row justify-content-center">
-      <div className="col mt-3">
-        <h2 className="text-center">Apa itu KIR ?</h2>
+const Content = loadable(() => import("./lazy/DeskripsiContent"), {
+  fallback: (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col mt-3">
+          <h2 className="text-center">Apa itu KIR ?</h2>
+        </div>
+      </div>
+      <div className="row justify-content-center mt-3 text-center">
+        <div className="col col-sm-5">
+          <p>
+            Kir adalah salah satu ekstrakurikuler di{" "}
+            <a href="https://smpn13kotabekasi.sch.id/">SMP Negeri 13 Bekasi</a>{" "}
+            yang bertemakan ekstrakurikuler yang menyediakan berbagai kegiatan
+            unik dan beragam, tentunya kegiatannya juga anti mainstream.
+          </p>
+        </div>
+        <div className="col col-sm-5">
+          <p>
+            Tidak ada senioritas di eksul kir ini, semua bebas berekspresi dan
+            berpendapat. Kalian bisa belajar dan bertanya-tanya ke kakak kelas
+            seputar hal sekolah ataupun luar sekolah.
+          </p>
+        </div>
       </div>
     </div>
-    <div className="row justify-content-center mt-3">
-      <div className="col col-sm-5">
-        <p>
-          Kir adalah salah satu ekstrakurikuler di{" "}
-          <a href="https://smpn13kotabekasi.sch.id/">SMP Negeri 13 Bekasi</a>{" "}
-          yang bertemakan ekstrakurikuler yang menyediakan berbagai kegiatan
-          unik dan beragam, tentunya kegiatannya juga anti mainstream.
-        </p>
-      </div>
-      <div className="col col-sm-5">
-        <p>
-          Tidak ada senioritas di eksul kir ini, semua bebas berekspresi dan
-          berpendapat. Kalian bisa belajar dan bertanya-tanya ke kakak kelas
-          seputar hal sekolah ataupun luar sekolah.
-        </p>
-      </div>
-    </div>
-  </div>
-);
+  ),
+});
 
 function Deskripsi() {
   return (
     <section id="deskripsi">
       <LazyLoad once>
-        <Suspense fallback={Fallback()}>
-          <Content />
-        </Suspense>
+        <Content />
       </LazyLoad>
     </section>
   );
--- src/App/Routing.js
+++ src/App/Routing.js
@@ -1,26 +1,23 @@
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import loadable from "@loadable/component";
-import { Suspense } from "react";
 
 const NotFound = loadable(() => import("./Components/NotFound"));
 const Covid = loadable(() => import("./Components/covid"));
 const Main = loadable(() => import("./Components/main"));
 
-const Navigasi = loadable(() => import("./Components/Navigasi"));
+const Navigasi = loadable(() => import("./Components/Navigasi"), {
+  fallback: (
+    <nav
+      className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
+      style={{ height: "56px" }}
+    />
+  ),
+});
 
 function Routing() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Suspense
-        fallback={
-          <nav
-            className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
-            style={{ height: "56px" }}
-          />
-        }
-      >
-        <Navigasi />
-      </Suspense>
+      <Navigasi />
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/covid" component={Covid} />
